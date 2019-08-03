4ba62d8536312ebf20ef1d78ea6df62800e489ea
hdknr <gmail@hdknr.com>
2019-08-03 17:35:04 +0900

    nullable age fields

--- database/migrations/2019_08_03_082457_add_age_to_user.php
+++ database/migrations/2019_08_03_082457_add_age_to_user.php
@@ -14,7 +14,7 @@ class AddAgeToUser extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->smallInteger('age')->nullable() ;
         });
     }
 
@@ -26,7 +26,7 @@ class AddAgeToUser extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('age');
         });
     }
 }
