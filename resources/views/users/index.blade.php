6801528fcd49451308b93a4dc8420ea07ee79b71
hdknr <gmail@hdknr.com>
2019-08-03 16:54:42 +0900

    List -> Show

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,7 +2,7 @@
     @foreach ($users as $user)
     <tr>
         <td>{{ $user->id }}</td>
-        <td><a href="#">{{ $user->name }}</a></td>
+        <td><a href="{{ url('users/'.$user->id) }}">{{ $user->name }}</a></td>
     </tr>
     @endforeach 
 </div>
