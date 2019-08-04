72bde9199c05df58f1c8763893bec6cefef512ca
hdknr <gmail@hdknr.com>
2019-08-04 12:38:55 +0900

    Todo.days_of_week

--- app/Todo.php
+++ app/Todo.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'every_monday', 'every_tuesday', 'every_wednesday', 
+        'every_thursday', 'every_friday', 'every_saturday', 'every_sunday',
+    ];
+
+    function startsWith($str, $prefix) {
+        return substr($str,  0, strlen($prefix)) === $prefix;
+    }
+
+    public function getDaysOfWeekAttribute() {
+        $columns = $this->getConnection()->getSchemaBuilder()->getColumnListing($this->getTable()); 
+        return array_values(array_filter($columns, function($i){
+            return $this->startsWith($i, 'every_') && $this->getAttribute($i) == true;
+        }));
+    }
 }
