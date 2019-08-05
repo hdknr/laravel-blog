89eead804bd1980c7d9175cefcd2277989753b29
hdknr <gmail@hdknr.com>
2019-08-05 10:12:02 +0900

    Todo saving Events

--- app/Todo.php
+++ app/Todo.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Todo extends Model
 {
@@ -11,6 +12,38 @@ class Todo extends Model
         'every_monday', 'every_tuesday', 'every_wednesday', 
         'every_thursday', 'every_friday', 'every_saturday', 'every_sunday',
     ];
+     protected static function boot()
+    {
+        // Eloquent Events
+        // https://laravel.com/docs/5.8/eloquent#events
+
+        parent::boot();
+
+        /*
+        self::creating(function($instance)
+        {
+            return $instance->onUpdating();
+        });
+
+        self::updating(function($instance){
+            return $instance->onUpdating();
+        });
+        */
+
+        self::saving(function($instance)
+        {
+            return $instance->onSaving();
+        });
+
+    }
+
+    function onSaving() {
+        $now = Carbon::now();
+        if ($this->start_at == null){
+           $this->start_at = $now; 
+        }
+        return true;            // false -> not save
+    }
 
     function startsWith($str, $prefix) {
         return substr($str,  0, strlen($prefix)) === $prefix;
