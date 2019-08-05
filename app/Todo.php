3a5146cb534fbb841c6b8d6754d86f811e493604
hdknr <gmail@hdknr.com>
2019-08-05 10:57:13 +0900

    Todo Carbon DateTime

--- app/Todo.php
+++ app/Todo.php
@@ -7,12 +7,15 @@ use Carbon\Carbon;
 
 class Todo extends Model
 {
+    // Meta
     protected $fillable = [
         'title',
         'every_monday', 'every_tuesday', 'every_wednesday', 
         'every_thursday', 'every_friday', 'every_saturday', 'every_sunday',
     ];
-     protected static function boot()
+    protected $dates = ['start_at', 'closed_at'];
+
+    protected static function boot()
     {
         // Eloquent Events
         // https://laravel.com/docs/5.8/eloquent#events
