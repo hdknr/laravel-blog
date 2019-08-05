dfe21e181b6e408316fd3666920c01c5e87f1c4f
hdknr <gmail@hdknr.com>
2019-08-05 15:39:59 +0900

    Todoを実行したときにsave() する前にupdateStartAt(true) で呼ぶ

--- app/Todo.php
+++ app/Todo.php
@@ -9,7 +9,7 @@ class Todo extends Model
 {
     // Meta
     protected $fillable = [
-        'title',
+        'title', 'start_at',
         'every_monday', 'every_tuesday', 'every_wednesday', 
         'every_thursday', 'every_friday', 'every_saturday', 'every_sunday',
     ];
@@ -22,36 +22,41 @@ class Todo extends Model
 
         parent::boot();
 
-        /*
         self::creating(function($instance)
         {
-            return $instance->onUpdating();
+            $now = Carbon::now();
+            if ($instance->start_at == null){
+                $instance->start_at = $now; 
+            }
+            $instance->updateStartAt(false);
+            return true;
         });
 
+        /*
         self::updating(function($instance){
             return $instance->onUpdating();
         });
         */
 
+        /*
         self::saving(function($instance)
         {
             return $instance->onSaving();
         });
+        */
 
     }
 
-    function onSaving() {
-        $now = Carbon::now();
-        if ($this->start_at == null){
-           $this->start_at = $now; 
-        }
-        return true;            // false -> not save
+    function shouldUpdateStartAt() {
+        return $this->is_closed == false && $this->is_executed;
     }
 
-    function updateStartAt() {
-        if ($this->start_at == null){
-            return;
-        }
+    function updateStartAt($processed) {
+        // 実行したときは $processed == true
+        $dow = $this->next_day_of_week;
+        $date = Carbon::parse("this ${dow}")->setTime($this->start_at->hour, $this->start_at->minute, $this->start_at->second);
+        $date = ($date->lt($this->start_at) || $processed) ? Carbon::parse("next ${dow}"): $date;
+        $this->start_at = $date->setTime($this->start_at->hour, $this->start_at->minute, $this->start_at->second);
     }
 
     function startsWith($str, $prefix) {
@@ -68,5 +73,27 @@ class Todo extends Model
     public function getIsPeriodicAttribute() {
         $days_of_week = $this->days_of_week;
         return $days_of_week && count($days_of_week) > 0;
+    }
+
+    public function getIsClosedAttribute() {
+        return $this->closed_at != null;
+    }
+
+    public function getIsExecutedAttribute() {
+        $now = Carbon::now();
+        return $this->start_at != null && $now->gte($this->start_at);
+    }
+
+    public function getNextDayOfWeekAttribute() {
+        if (!$this->is_periodic) {
+            return null;
+        }
+        $dow = strtolower($this->start_at->englishDayOfWeek);
+        $days_of_week = array_merge($this->days_of_week, $this->days_of_week);
+
+        $i = array_search('every_'.$dow, $days_of_week);
+        $i = ($i === false) ? 0 : ($i + 1);
+       
+        return str_replace('every_', '', $days_of_week[$i]);
     }
 }
